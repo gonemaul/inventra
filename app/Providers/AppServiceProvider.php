0db6b179d6c799d6a70320b763ade8cee3fa753b
gonemaul <wongdesoo366@gmail.com>
2025-12-10 17:06:07 +0700

    update: backup to drive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Storage;
+use League\Flysystem\Filesystem;        // <--- Tambahkan ini
+use Illuminate\Filesystem\FilesystemAdapter; // <--- Tambahkan ini
+use Masbug\Flysystem\GoogleDriveAdapter; // <--- Tambahkan ini
+use Google\Service\Drive;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +25,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        try {
+            Storage::extend('google', function ($app, $config) {
+                $client = new \Google\Client();
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $service = new Drive($client);
+
+                // $folderId = !empty($config['folder']) ? $config['folder'] : 'root';
+                $folderId = 'BACKUP_INVENTRA';
+                // 'useHasDir' => true membantu adapter membedakan folder dan file
+                $options = [
+                    // 'useHasDir' => true,
+                ];
+
+                $adapter = new GoogleDriveAdapter($service, $folderId, $options);
+                $driver = new Filesystem($adapter);
+
+                return new FilesystemAdapter($driver, $adapter);
+            });
+        } catch (\Exception $e) {
+            // Log error jika driver gagal dimuat, agar aplikasi tidak crash total
+            // Log::error("Gagal memuat driver Google Drive: " . $e->getMessage());
+        }
         Vite::prefetch(concurrency: 3);
     }
 }
