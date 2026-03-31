2e9ef0625b82384d98afc0b6d22494ece020b3f7
gonemaul <wongdesoo366@gmail.com>
2026-03-31 07:15:28 +0700

    build: update production build assets and manifest

---
+++ public/build/assets/InputError-Dn5bbf_0.js
@@ -0,0 +1,1 @@
+import{O as s,a0 as t,I as a,z as r,C as o,K as c}from"./vendor-core-CVHjoCj7.js";const n={class:"text-sm text-red-600"},l={__name:"InputError",props:{message:{type:String}},setup(e){return(i,m)=>s((r(),a("div",null,[o("p",n,c(e.message),1)],512)),[[t,e.message]])}};export{l as _};
---
+++ public/build/assets/useActionLoading-CkFrDjc9.js
@@ -0,0 +1,1 @@
+import{r as o}from"./vendor-core-CVHjoCj7.js";const n=o(!1);function r(){return{isActionLoading:n}}export{r as u};
